cbe9dc71b8b8dd24a11e795a61acf422fa223b00
chetanr250 <=>
2024-08-11 00:40:27 +0530

    Redesigned UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,7 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List<String> filters = ['Helping', 'Need Help'];
 
   bool locationLoaded = false;
-  late LatLng? currentLocation;
+  late LatLng? currentLocation = LatLng(0, 0);
   late GoogleMapController mapController;
   Completer<GoogleMapController> controllerCompleter = Completer();
 
@@ -28,6 +28,18 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     getCurrentLocation();
+  }
+
+  Widget helpOrSeek(isHelping, currentLocation, selectedFilter) {
+    return Positioned(
+      right: 20.0,
+      bottom: 180.0,
+      child: buildFloatingActionButton(
+        isHelping: isHelping,
+        currentLocation: currentLocation,
+        selectedFilter: selectedFilter,
+      ),
+    );
   }
 
   Widget recenterButton(Completer<GoogleMapController> controllerCompleter,
@@ -38,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
         if (snapshot.hasData) {
           return Positioned(
             right: 20.0,
-            bottom: 30.0,
+            bottom: 110.0,
             child: FloatingActionButton(
               mini: false,
               backgroundColor: Colors.white,
@@ -69,12 +81,13 @@ class _HomeScreenState extends State<HomeScreen> {
       currentLocation = LatLng(position.latitude, position.longitude);
 
       currentPosition = CameraPosition(target: currentLocation!, zoom: 15.0);
+      locationLoaded = true;
     });
-    Future.delayed(Duration(milliseconds: 1000), () {
-      setState(() {
-        locationLoaded = true;
-      });
-    });
+    // Future.delayed(Duration(milliseconds: 1000), () {
+    //   setState(() {
+
+    //   });
+    // });
   }
 
   Widget buildFloatingActionButton({
@@ -86,7 +99,11 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: isHelping
           ? Color.fromARGB(255, 137, 202, 255)
           : Color.fromARGB(255, 250, 121, 121),
-      onPressed: () {},
+      onPressed: () {
+        print('Helping: $isHelping');
+        print('Location: $currentLocation');
+        print('Filter: $selectedFilter');
+      },
       child: Icon(
         isHelping
             ? FontAwesomeIcons.handHoldingHeart
@@ -112,22 +129,25 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(16.0),
 
         child: BottomAppBar(
-            child: ToggleSwitch(
-          initialLabelIndex: isHelping ? 0 : 1,
-          minWidth: 100.0,
-          cornerRadius: 20.0,
-          activeBgColor: const [Colors.blue],
-          borderWidth: 2.0,
-          customWidths: const [200.0, 200.0],
-          activeFgColor: Colors.white,
-          inactiveBgColor: Colors.grey,
-          inactiveFgColor: Colors.white,
-          fontSize: 16,
-          labels: filters,
-          onToggle: (index) {
-            print('switched to: $index');
-          },
-        )),
+          child: ToggleSwitch(
+            initialLabelIndex: isHelping ? 0 : 1,
+            minWidth: 100.0,
+            cornerRadius: 20.0,
+            activeBgColor: const [Colors.blue],
+            borderWidth: 2.0,
+            customWidths: const [200.0, 200.0],
+            activeFgColor: Colors.white,
+            inactiveBgColor: Colors.grey,
+            inactiveFgColor: Colors.white,
+            fontSize: 16,
+            labels: filters,
+            onToggle: (index) {
+              setState(() {
+                isHelping = index == 0;
+              });
+            },
+          ),
+        ),
       ),
       body: Stack(
         children: [
@@ -143,7 +163,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   zoomControlsEnabled: false,
                 )
               : Center(child: CircularProgressIndicator()),
-          recenterButton(controllerCompleter, currentLocation!)
+          recenterButton(controllerCompleter, currentLocation!),
+          helpOrSeek(
+              isHelping, currentLocation, isHelping ? filters[0] : filters[1]),
         ],
       ),
     );
