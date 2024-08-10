e134aac510d684eb4e153037c6ced47681165e7c
chetanr250 <=>
2024-08-10 12:58:40 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/disaster_ready/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.disaster_ready
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Disaster Ready'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Disaster Ready!'),
+      ),
+    );
+  }
+}
